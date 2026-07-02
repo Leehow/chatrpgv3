bb9d2fe4f786af0c8c3a5cf63459a4e936e8e7d9
Leehow <Leeehow@gmail.com>
2026-07-02 10:36:55 +0800

    runtime: expose clue and handout engines

--- src/chatrpg/runtime/__init__.py
+++ src/chatrpg/runtime/__init__.py
@@ -1,6 +1,8 @@
 from chatrpg.runtime.adventure import AdventureEngine
 from chatrpg.runtime.characters import CharacterEngine
+from chatrpg.runtime.clues import ClueAcquisitionEngine
 from chatrpg.runtime.dice import DiceEngine, DiceRequest, DiceResult
+from chatrpg.runtime.handouts import HandoutEngine
 from chatrpg.runtime.knowledge import KnowledgeEngine
 from chatrpg.runtime.procedure import ProcedureEngine
 from chatrpg.runtime.rules import RulesEngine
@@ -10,9 +12,11 @@ from chatrpg.runtime.visibility import VisibilityEngine
 __all__ = [
     "AdventureEngine",
     "CharacterEngine",
+    "ClueAcquisitionEngine",
     "DiceEngine",
     "DiceRequest",
     "DiceResult",
+    "HandoutEngine",
     "KnowledgeEngine",
     "ProcedureEngine",
     "RulesEngine",
