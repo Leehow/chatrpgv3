17b5ef9d1de3e407f845b1044ba03a50262e1caa
Leehow <Leeehow@gmail.com>
2026-07-02 10:38:58 +0800

    ir: expose expanded IR and validators

--- src/chatrpg/ir/__init__.py
+++ src/chatrpg/ir/__init__.py
@@ -1,15 +1,24 @@
-from chatrpg.ir.adventure import AdventureIR
+from chatrpg.ir.adventure import AdventureIR, ClueCarrier, ContentUnit, HandoutAsset, Revelation
 from chatrpg.ir.events import DomainEvent
-from chatrpg.ir.ruleset import RulesetIR
+from chatrpg.ir.ruleset import ProcedureSpec, RulesetIR
 from chatrpg.ir.source import SourceBlock, SourceDocument, SourceRef
 from chatrpg.ir.state import SessionState
+from chatrpg.ir.validators import AdventureIRValidator, RulesetIRValidator, ValidationIssue
 
 __all__ = [
     "AdventureIR",
+    "AdventureIRValidator",
+    "ClueCarrier",
+    "ContentUnit",
     "DomainEvent",
+    "HandoutAsset",
+    "ProcedureSpec",
+    "Revelation",
     "RulesetIR",
+    "RulesetIRValidator",
     "SessionState",
     "SourceBlock",
     "SourceDocument",
     "SourceRef",
+    "ValidationIssue",
 ]
