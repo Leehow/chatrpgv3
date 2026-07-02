9eeffe118285c4a54c85479979343f8d82300b29
Leehow <Leeehow@gmail.com>
2026-07-02 15:04:06 +0800

    runtime: expose formula-backed character creation APIs

--- src/chatrpg/runtime/__init__.py
+++ src/chatrpg/runtime/__init__.py
@@ -1,7 +1,9 @@
 from chatrpg.runtime.adventure import AdventureEngine
+from chatrpg.runtime.character_creation import CharacterCreationEngine
 from chatrpg.runtime.characters import CharacterEngine
 from chatrpg.runtime.clues import ClueAcquisitionEngine
 from chatrpg.runtime.dice import DiceEngine, DiceRequest, DiceResult
+from chatrpg.runtime.formulas import FormulaEvaluator
 from chatrpg.runtime.handouts import HandoutEngine
 from chatrpg.runtime.knowledge import KnowledgeEngine
 from chatrpg.runtime.procedure import ProcedureEngine
@@ -11,11 +13,13 @@ from chatrpg.runtime.visibility import VisibilityEngine
 
 __all__ = [
     "AdventureEngine",
+    "CharacterCreationEngine",
     "CharacterEngine",
     "ClueAcquisitionEngine",
     "DiceEngine",
     "DiceRequest",
     "DiceResult",
+    "FormulaEvaluator",
     "HandoutEngine",
     "KnowledgeEngine",
     "ProcedureEngine",
