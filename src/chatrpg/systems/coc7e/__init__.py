e2ccdc4b673fe9bbda69dd9970b6753599e869d3
Leehow <Leeehow@gmail.com>
2026-07-02 10:00:32 +0800

    systems: export coc7e resolution result models

--- src/chatrpg/systems/coc7e/__init__.py
+++ src/chatrpg/systems/coc7e/__init__.py
@@ -1,3 +1,17 @@
-from chatrpg.systems.coc7e.runtime import CocSanityResult, CocSkillRollResult, Coc7eEngine
+from chatrpg.systems.coc7e.runtime import (
+    CocD100Roll,
+    CocLuckSpendResult,
+    CocOpposedResult,
+    CocSanityResult,
+    CocSkillRollResult,
+    Coc7eEngine,
+)
 
-__all__ = ["Coc7eEngine", "CocSanityResult", "CocSkillRollResult"]
+__all__ = [
+    "Coc7eEngine",
+    "CocD100Roll",
+    "CocLuckSpendResult",
+    "CocOpposedResult",
+    "CocSanityResult",
+    "CocSkillRollResult",
+]
