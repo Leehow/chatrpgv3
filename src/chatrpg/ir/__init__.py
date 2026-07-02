a7a4f9585347e734d8ded3d8d5337b4537b8cf33
Leehow <Leeehow@gmail.com>
2026-07-02 08:38:45 +0800

    code: expose IR models

---
+++ src/chatrpg/ir/__init__.py
@@ -0,0 +1,15 @@
+from chatrpg.ir.adventure import AdventureIR
+from chatrpg.ir.events import DomainEvent
+from chatrpg.ir.ruleset import RulesetIR
+from chatrpg.ir.source import SourceBlock, SourceDocument, SourceRef
+from chatrpg.ir.state import SessionState
+
+__all__ = [
+    "AdventureIR",
+    "DomainEvent",
+    "RulesetIR",
+    "SessionState",
+    "SourceBlock",
+    "SourceDocument",
+    "SourceRef",
+]
